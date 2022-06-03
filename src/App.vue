b5cf8a748a299ef32025659b51ab28fb78a5dfd3
Matthieu Mota <matthieumota@gmail.com>
2022-06-03 11:36:59 +0200

    Remove todo and complete todo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <input class="new-todo" placeholder="What needs to be done?" autofocus v-model="newTodo" @keyup.enter="addTodo" />
     </header>
     <!-- This section should be hidden by default and shown when there are todos -->
-    <section class="main">
+    <section class="main" v-if="todos.length > 0">
       <input id="toggle-all" class="toggle-all" type="checkbox" />
       <label for="toggle-all">Mark all as complete</label>
       <ul class="todo-list">
@@ -13,16 +13,16 @@
         <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
         <li v-for="(todo, index) in todos" :key="index" :class="{ completed: todo.state }">
           <div class="view">
-            <input class="toggle" type="checkbox" :checked="todo.state" />
+            <input class="toggle" type="checkbox" :checked="todo.state" v-model="todo.state" />
             <label>{{ todo.name }}</label>
-            <button class="destroy"></button>
+            <button class="destroy" @click="removeTodo(todo)"></button>
           </div>
           <input class="edit" value="Rule the web" />
         </li>
       </ul>
     </section>
     <!-- This footer should be hidden by default and shown when there are todos -->
-    <footer class="footer">
+    <footer class="footer" v-if="todos.length > 0">
       <!-- This should be `0 items left` by default -->
       <span class="todo-count"><strong>{{ todos.length }}</strong> item(s) left</span>
       <!-- Remove this if you don't implement routing -->
@@ -58,6 +58,9 @@ export default {
     addTodo() {
       this.todos.push({ name: this.newTodo, state: false });
       this.newTodo = '';
+    },
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
     },
   }
 };
